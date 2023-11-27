3ab78d5a2d55069a43e5c84a4a7d37cc6d8f9f6a
M3hmetCiftci <Mehmet.cift27@gmail.com>
2023-11-27 19:03:13 +0300

    Console_App

---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object_Oriented_Programming_method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //    Program p = new Program();
+            //    p.yazdir("------------C# Dersleri-------------------");
+            //    Console.WriteLine();
+
+            //    int sonuc = p.Topla(1, 10);
+            //    Console.WriteLine("Toplam = " + sonuc);
+            //    Console.WriteLine();
+
+            //    int CarpimSonuc = Carpim(2, 4);
+            //    Console.WriteLine("Çarpım Sonucu = " + CarpimSonuc);
+            //    Console.ReadKey();
+
+            //}
+            //private void yazdir(string input)
+            //{
+            //    Console.WriteLine(input);
+            //}
+            //private int Topla(int x, int y)
+            //{
+            //    return x + y;
+            //}
+            //static int Carpim(int j, int k)
+            //{
+            //    return j * k;
+            //}
+
+            //<-------------------------------------------------------------------------------------->
+
+            //int i = 5;
+            //method(ref i);
+            //Console.WriteLine("Sonuc = "+i);
+            //private static void method(ref int i)
+            //}
+            //{
+            //public static void method(ref int i)
+            //    i = 100;
+            //}
+
+            //<-------------------------------------------------------------------------------------->
+
+
+            //    int toplam, carpim;
+            //    Hesap(10, 5, out toplam, out carpim);
+            //    Console.WriteLine($" toplam değeri = {toplam}  Çapım değeri = {carpim}");
+            //}
+
+
+            //private static void Hesap(int ilksayi,int ikincisayi,out int toplam,out int carp)
+            //{
+            //    toplam = ilksayi + ikincisayi;
+            //    carp =ilksayi * ikincisayi;
+            //}
+
+
+
+
+            //     //<--------------------------------------------------------------------------------------> 
+            //    //int[] _Numbers = { 1, 2, 3, 4, 5, };
+            //    //ParametreMothod(_Numbers);
+            //    //&
+            //    ParametreMothod(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14);
+            //}
+            //public static void ParametreMothod(params int[]Numbers)
+            //{
+            //    foreach (var item in Numbers)
+            //    {
+            //Console.WriteLine(item);
+            //    }
+            //}
+
+            //     //<--------------------------------------------------------------------------------------> 
+            //toplam(10, 20);
+            //toplam(10, 20, 30);
+            //toplam(10.1f,10.1f);
+            //toplam(30, 70,out int toplama);
+            //}
+            // static void toplam(int a, int b)
+            //{
+            //    int toplama = a + b;
+            //    Console.WriteLine($"ilk toplam sonucu = {toplama}");
+            //}
+            //static void toplam(int a, int b,int c)
+            //{
+            //    int toplama = a + b+c;
+            //    Console.WriteLine($"ikinci toplam sonucu = {toplama}");
+            //}
+            //static void toplam(float a, float b)
+            //{
+            //    float toplama = a + b;
+            //    Console.WriteLine($"ücüncü toplam sonucu {toplama}");
+            //}
+            //static void toplam(int a, int b, out int topla)
+            //{
+            //    topla = a + b;
+            //    Console.WriteLine($"dürdüncü toplam sunucu = {topla}");
+            //}
+        }
+    }
+}
